25e3c8d47bc177163d4395938b87454498eb07ff
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-18 19:04:02 +0530

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// This program is to understand basic use of RELATIONAL OPERATORS.
+// This program is to understand conditional statements(if,else,switch)
 int main() {
     int age1;
-    int age2;
-    cout <<"Enter the age of first person : ";
+    cout << "Enter your age: ";
     cin >> age1;
-    cout <<"Enter the age of second person : ";
-    cin >> age2;
-    if (age1>age2) {
-        cout << "Person 1 is older then Person 2";
+    if (age1>=18) {
+        cout<<"Your age if above 18 years so, you can use this calculater"<<endl;
+        int x,y;
+        cout<<"Enter the value of x and y."<<endl;
+        cin>>x>>y;
+        cout<<"your numbers are "<<x<<" and "<<y<<endl;
+        int a;
+        cout<<"Enter 1 for sum 1 for sub and 3 for mul"<<endl;
 
-    }
-    else if (age1<age2) {
-        cout << "Person 2 is older then Person 1";
+        cout<<"enter your choice : ";
+        cin>>a;
+        cout<<"your choice is "<<a<<endl;
+        int add = x+y;
+        int sub = x-y;
+        int mul = x*y;
+        switch (a) {
+
+            case 1:
+                cout<<"Your sum is "<<add<<endl;
+                break;
+            case 2:
+                cout<<"Your sub is "<<sub<<endl;
+                break;
+            case 3:
+                cout<<"Your mul is "<<mul<<endl;
+                break;
+
+        }
 
     }
     else {
-        cout <<"Both are of same age.";
+        cout<<"You are not old enough to use the calculator.";
+
     }
 
 
