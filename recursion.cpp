4ffb146f1779d58409c6f092f334d37eb4258afa
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-21 13:16:43 +0530

    final commit

--- recursion.cpp
+++ recursion.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
+/*
 void facto(int n) {
     int fa=1;
     for (int i = n; i >0; i--) {
@@ -15,4 +15,33 @@ int main() {
     cout<<"Enter the number for factorial: ";
     cin>>a;
     facto(a);
+}
+*/
+/*
+void pr(int start,int end) {
+    if (start>end) return;
+    cout << start << " ";
+    pr(start+1,end);
+}
+int main() {
+    int x;
+    cout<<"Enter the number: ";
+    cin>>x;
+    pr(1,x);
+}
+*/
+
+int soa(int n) {
+    if (n==0)return 1;
+    int l=n%10;
+    int m=n/10;
+    cout<<l<<" ";
+    return l + soa(m);
+}
+int main() {
+    int x;
+    cout<<"Enter the number : ";
+    cin>>x;
+    int result = soa(x);
+    cout << "\nSum of digits: " << result << endl;
 }
